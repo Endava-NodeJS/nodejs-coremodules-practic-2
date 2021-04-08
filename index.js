90d087b4b5d185347ec7cf753d041a095834dfbd
Gheorghe Cecoi <Gheorghe.Cecoi@endava.com>
2021-04-08 14:25:22 +0300

    created server

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const DB = require('./db');
-const db = new DB('./notes.json');
+// const DB = require('./db');
+// const db = new DB('./notes.json');
+const http = require('http');
+// console.log(db.get());
 
-console.log(db.get());
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('Hello world');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
