481158fb94602affbcb24fc8d198f7d58b6b45a1
Nicoleta Ursu <nicoleta.ursu@endava.com>
2021-04-08 15:14:08 +0300

    Add get and post

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// const DB = require('./db');
-// const db = new DB('./notes.json');
+const DB = require('./db');
+
+const db = new DB('./notes.json');
+
+
 const http = require('http');
-// console.log(db.get());
+const { endpoints } = require('./controller')
+
+console.log(db.get());
 
 const requestListener = function (req, res) {
+    if (endpoints.has(req.url)) {
+      endpoints.get(req.url)[req.method.toLowerCase()](req, res)
+    }
+
     res.writeHead(200);
     res.end('Hello world');
 }
